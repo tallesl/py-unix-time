6cce96196e2ef0a67e0952b5400d98e52b75e015
Talles L <talleslasmar@gmail.com>
2019-09-10 09:33:13 -0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(name='unix-time',
+      version='1.0.0',
+      url='https://github.com/tallesl/py-unix-time',
+      author='Talles Lasmar',
+      author_email='talleslasmar@gmail.com',
+      description='Unix timestamp made easy.',
+      long_description=open('README.md', 'r').read(),
+      long_description_content_type='text/markdown',
+      packages=find_packages())

2dfb0f67f9a6f3a4521a6d1eb29785628fab5843
Talles L <talleslasmar@gmail.com>
2019-09-30 17:07:16 -0300

    calculating on datetimes and supporting dates

--- unix_time/__init__.py
+++ unix_time/__init__.py
@@ -1,17 +1,27 @@
-from datetime import datetime
+from datetime import date, datetime, timedelta
+from typing import Union
 
 
-def ts_date(_ts: int) -> int:
-    return int(_ts / 86400) * 86400
+def ts(dt: Union[date, datetime]) -> int:
+    return int(_to_datetime(dt).timestamp())
+
+
+def ts_date(dt: datetime) -> int:
+    return ts(dt.replace(hour=0, minute=0, second=0, microsecond=0))
 
 
 def ts_now() -> int:
-    return int(datetime.now().timestamp())
+    return ts(datetime.now())
 
 
 def ts_today() -> int:
-    return ts_date(ts_now())
+    return ts(datetime.today())
 
 
 def ts_yesterday() -> int:
-    return ts_today() - 86400
+    return ts(date.today() - timedelta(days=1))
+
+
+# https://stackoverflow.com/a/1937631
+def _to_datetime(dt: Union[date, datetime]) -> datetime:
+    return datetime(*dt.timetuple()[:6]) if type(dt) is date else dt
